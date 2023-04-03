7e4b07af634b4d528c2cf4bd67e68dbab2fb0858
Erik O Gabrielsson <erik.o.gabrielsson@sectra.com>
2023-04-03 08:18:46 +0200

    Add Metadata to main __init__

--- opentile/__init__.py
+++ opentile/__init__.py
@@ -1,4 +1,4 @@
-#    Copyright 2021 SECTRA AB
+#    Copyright 2021-2023 SECTRA AB
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -13,5 +13,6 @@
 #    limitations under the License.
 
 from opentile.opentile import OpenTile
+from opentile.metadata import Metadata
 
 __version__ = "0.8.1"
