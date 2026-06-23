9bd7e7cc190f21dca5c22ca35e320290ad1d3c85
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2026-06-23 16:36:45 +0200

    Use Time Zone info whenever available

--- opentile/formats/svs/svs_metadata.py
+++ opentile/formats/svs/svs_metadata.py
@@ -14,7 +14,7 @@
 
 """Metadata parser for svs files."""
 
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from typing import Any, Optional
 
 from tifffile import TiffPage
@@ -34,14 +34,55 @@ class SvsMetadata(Metadata):
         except (KeyError, ValueError):
             return None
 
+    def _get_timezone(self) -> Optional[timezone]:
+        if "Time Zone" in self._svs_metadata:
+            tz_str = self._svs_metadata["Time Zone"]
+            # as of today, we have assert tz_str.startswith("GMT") == True
+            # Example tz_str: "GMT-07:00" or "GMT-0400"
+            if not (tz_str.startswith("GMT") and tz_str[3] in ("+", "-")):
+                return None
+            sign = -1 if tz_str[3] == "-" else 1
+            digits = tz_str[4:].replace(":", "")  # "06:00" or "0600" -> "0600"
+            hours, minutes = map(int, (digits[:2], digits[2:]))
+            return timezone(sign * timedelta(hours=hours, minutes=minutes))
+
+    @staticmethod
+    def _extract_date(date_string):
+        """
+        Extract date from either 'MM/DD/YYYY' or 'MM/DD/YY' format.
+
+        Args:
+            date_string: Date string in format 'MM/DD/YYYY' or 'MM/DD/YY'
+
+        Returns:
+            datetime object
+
+        Raises:
+            ValueError: If date string doesn't match expected formats
+        """
+        # Try 4-digit year first (MM/DD/YYYY)
+        try:
+            return datetime.strptime(date_string, "%m/%d/%Y")
+        except ValueError:
+            pass
+
+        # Try 2-digit year (MM/DD/YY)
+        try:
+            return datetime.strptime(date_string, "%m/%d/%y")
+        except ValueError:
+            raise ValueError(  # noqa: B904
+                f"Date '{date_string}' doesn't match expected formats (MM/DD/YYYY or MM/DD/YY)"
+            )
+
     @property
     def aquisition_datetime(self) -> Optional[datetime]:
         try:
-            date = datetime.strptime(self._svs_metadata["Date"], r"%m/%d/%y")
+            date = SvsMetadata._extract_date(self._svs_metadata["Date"])
             time = datetime.strptime(self._svs_metadata["Time"], r"%H:%M:%S")
+            tz_info = self._get_timezone()
         except (KeyError, ValueError):
             return None
-        return datetime.combine(date, time.time())
+        return datetime.combine(date, time.time(), tzinfo=tz_info)
 
     @property
     def mpp(self) -> float:
